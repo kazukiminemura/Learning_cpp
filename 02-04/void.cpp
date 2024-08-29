59d73adbfd7ba91f80091129a65e3c784599727c
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-08-29 11:37:48 +0900

    added

---
+++ 02-04/void.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int f(int x, int y){
+  return 2 * x + y;
+}
+
+void Show(int x, int y){
+  cout << "f(" << x << ", " << y << ") =" << f(x, y) << endl;
+}
+
+
+int main(){
+  Show(1, 2);
+  Show(182, 144);
+  
+  return 0;
+}
---
+++ 02-05/global_local_variables_fixed.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int a = 0;
+
+void Inc(){
+  int b = 0;
+  ++a;
+  ++b;
+  cout << a << " <- a | b -> " << b << endl;
+}
+
+int main(){
+  Inc();
+  Inc();
+  Inc();
+    
+  return 0;
+}
