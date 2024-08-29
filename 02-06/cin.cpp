510ba9d110113deff9e215d6590d227adb11bbee
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-08-29 21:31:21 +0900

    updated

--- 02-06/cin.cpp
+++ 02-06/cin.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
   int a;
 
-  cout << "enter interger value" << endl;
+  cout << "enter interger value > " << flush;
   cin >> a;
 
   cout << "the reminder when the input value divided by three " << a % 3 << endl;
