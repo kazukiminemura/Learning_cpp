4301768043d4bee32ef77f88152497d630424f8a
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-09-03 19:39:36 +0900

    added

---
+++ 04-04/pointer_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Init(int array[]){
+  for(int i=0; i<5; ++i){
+    array[i] = i * 5;
+  }
+}
+
+void Show(int array[]){
+  for(int i=0; i<5; ++i){
+    cout << array[i] << ' ';
+  }
+  cout << endl;
+}
+
+int main(){
+  int n[5];
+  
+  Init(n);
+  Show(n);
+
+  return 0;
+}
