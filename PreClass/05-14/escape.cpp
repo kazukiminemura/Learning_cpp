727c1692985a6a8ae02355567107bb4dc651f83d
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-09-16 10:48:27 +0900

    added

--- PreClass/05-14/escape.cpp
+++ PreClass/05-14/escape.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void DumpCode(const char* str){
   for(int i=0; str[i] != '\0'; ++i){
-    printf("%02X ", (unsigned char) str[i]);
+    printf("%02x ", (unsigned char) str[i]);
   }
   cout << endl;
 }
