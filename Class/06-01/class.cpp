c5aa6c8ecc8ef49062a256b0a7504fa01f3b6b5d
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-09-19 21:58:16 +0900

    added

---
+++ Class/06-01/class.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_NAME = 16;
+
+class Student {
+  public:
+    char name[MAX_NAME+1];
+    int scoreJapanese;
+    int scoreMath;
+    int scoreEnglish;
+
+    void Show();
+};
+
+void Student::Show(){
+  cout << "name : " << name << endl
+      << " Japanese : " << scoreJapanese << " point"
+      << " Math : " << scoreMath << " point"
+      << " English : " << scoreEnglish << " point" << endl;
+}
+
+
+int main(){
+  Student student[] = {
+    { "Akai Takashi", 73, 98, 86},
+    { "Sasai Daisuke", 64, 45, 40},
+    { "Yoshida Kaoru", 76, 78, 9},
+  };
+
+  int size = sizeof student / sizeof * student;
+
+  for(int i=0; i<size; ++i){
+    student[i].Show();
+  }
+}
+
