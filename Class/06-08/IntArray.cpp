084c6694d5716d593ec7e6843fe3446bf8226fd9
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-09-21 19:42:28 +0900

    added

---
+++ Class/06-08/IntArray.cpp
@@ -0,0 +1,60 @@
+#include "IntArray.hpp"
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+// Access member values
+int IntArray::Get(int i){
+  CheckIndex(i);
+  return m_array[i];
+}
+
+void IntArray::Set(int i, int value){
+  CheckIndex(i);
+  m_array[i] = value;
+}
+
+// check index
+void IntArray::CheckIndex(int i){
+  if(0 <= i && i < m_size){
+    // index is valid
+  } else {
+    cout << "the index is out of bound" << endl
+        << "value : " << i << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+// constructor
+IntArray::IntArray(int size){
+  // fill_n is a function filling array with the specified value
+  m_array = new int[size];
+  m_size = size;
+
+  cout << "called constructor"
+      << "the number of elements is " << m_size << endl;
+}
+
+// destructor
+IntArray::~IntArray(){
+  delete [] m_array;
+
+  cout << "called destructor"
+      << "the number of elements was " << m_size << endl;
+}
+
+int IntArray::Size(){
+    return m_size;
+}
+
+// copy _Enable_default_constructor
+IntArray::IntArray(const IntArray& other){
+  m_array = new int[other.m_size];
+  m_size = other.m_size;
+
+  // copy is like a copying data on the memory
+  copy(other.m_array, other.m_array + m_size, m_array);
+
+  cout << "called copy constructor" << endl;
+}
