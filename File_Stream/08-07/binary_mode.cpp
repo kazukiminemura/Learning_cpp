4dda1b093544a8297cc50ea67464c40d5e1254c4
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-10-07 22:56:46 +0900

    added

---
+++ File_Stream/08-07/binary_mode.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+int main(){
+  int n = 214;
+  int n_16b = 0xD6;
+  int n_8b = 0326;
+
+  cout << n << endl;
+  cout << n_16b << endl;
+  cout << n_8b << endl;
+
+  return 0;
+}
+
+
