dfd092294605c41460a64a2c677ba78487ef1591
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-10-08 15:54:55 +0900

    added

--- File_Stream/08-08/binary_read_write.cpp
+++ File_Stream/08-08/binary_read_write.cpp
@@ -11,9 +11,19 @@ int main(){
     return EXIT_FAILURE;
   }
 
-  int n = 0x41424344;
-  file.write((const char*)&n, sizeof n);
+  int n_w = 0x41424344;
+  file.write((const char*)&n_w, sizeof n_w);
   file.close();
+
+  file.open("binary.txt", ios::in | ios::binary);
+  if(! file.is_open()){
+    return EXIT_FAILURE;
+  }
+
+  int n_r;
+  file.read((char*)&n_r, sizeof n_r);
+  file.close();
+  cout << n_r << endl;
 
   return 0;
 }
