8d44fda9987ff85441cfb3ca01dde73dbd7a4f40
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-10-15 21:00:43 +0900

    added

--- Template/09-03/template.hpp
+++ Template/09-03/template.hpp
@@ -1,6 +1,8 @@
 #ifndef TEMPLATE_H_
 #define TEMPLATE_H_
 
+// template should be seen from calling point.
+// for example, all of template should be written in a single header file
 template <typename TYPE>
   TYPE Abs(TYPE a)
 {
