c90089a10ac8daf4c8330be1d88d2406a003d584
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-11-01 20:23:23 +0900

    added

---
+++ Advanced/11-08/op_overload.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class IntArray {
+  public:
+    IntArray(int size);
+    ~IntArray();
+    IntArray(const IntArray& other); // copy constructor
+
+  public:
+    int Get(int i);
+    void Set(int i, int value);
+    int Size();
+    int& At(int i);
+    int& operator[] (int i);
+    const int& operator[] (int i) const;
+
+  private:
+    void CheckIndex(int i) const;
+
+  private:
+    int* m_array;
+    int m_size;
+};
+
+// Access member values
+int IntArray::Get(int i) {
+  CheckIndex(i);
+  return m_array[i];
+}
+
+void IntArray::Set(int i, int value){
+  CheckIndex(i);
+  m_array[i] = value;
+}
+
+// check index
+void IntArray::CheckIndex(int i) const {
+  if(0 <= i && i < m_size){
+    // index is valid
+  } else {
+    cout << "the index is out of bound" << endl
+        << "value : " << i << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+int& IntArray::At(int i) {
+  CheckIndex(i);
+  return m_array[i];
+}
+
+// this enables "cin >> v[i] or cout << v[i]"
+int& IntArray::operator[] (int i) {
+  CheckIndex(i);
+  return m_array[i];
+}
+
+const int& IntArray::operator[] (int i) const {
+  CheckIndex(i);
+  return m_array[i];
+}
+
+// constructor
+IntArray::IntArray(int size){
+  // fill_n is a function filling array with the specified value
+  m_array = new int[size];
+  m_size = size;
+
+  cout << "called constructor"
+      << "the number of elements is " << m_size << endl;
+}
+
+// destructor
+IntArray::~IntArray(){
+  delete [] m_array;
+
+  cout << "called destructor"
+      << "the number of elements was " << m_size << endl;
+}
+
+int IntArray::Size(){
+    return m_size;
+}
+
+
+int main(){
+
+  return 0;
+}
+
