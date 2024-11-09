135c38e61d5fe44ae56a5fe70775636d2338ce41
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-11-09 23:06:30 +0900

    added

---
+++ AdvancedAdvanced/12-02/or_xor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+  int a = 01111010;
+  int b = 00001111;
+  int c;
+
+  c = a | b;
+  cout << "a: " << bitset<8>(a) << endl;
+  cout << "b: " << bitset<8>(b) << endl;
+  cout << "c = a | b: " << bitset<8>(c) << endl;
+
+  c = a ^ b;
+  cout << "c = a ^ b: " << bitset<8>(c) << endl;
+
+
+  return 0;
+}
+
