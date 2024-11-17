1d695242e761eda3bb20d01349e6b08ab4245cb0
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-11-17 21:06:35 +0900

    added

--- AdvancedAdvanced/12-10/macro.cpp
+++ AdvancedAdvanced/12-10/macro.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 #define FIVE_TIMES for(int i_ = 0; i_ < 5; ++i_)
+#define TIMES(cnt, n) for(int cnt = 0; cnt < n; ++cnt) // macro function
 
 int main(){
   FIVE_TIMES {
     cout << "Hello" << endl;
   }
+
+  TIMES(i, 5){
+    cout << i << ' ';
+  }
+  cout << endl;
 
   return 0;
 }
