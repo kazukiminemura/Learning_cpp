ecd51b0f28f93a8de1b1b989c9d9b90b7afed6e8
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-11-23 18:00:37 +0900

    added

---
+++ Deeper/13-01/multi_inherit.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class ABase {
+public:
+  virtual ~ABase() {}
+  virtual void AHoge() = 0;
+};
+
+class BBase {
+public:
+  virtual ~BBase() {}
+  virtual BBase() = 0;
+};
+
+class C:
+  public ABase,
+  public BBase
+{
+  public:
+    virtual void AHoge() {
+      cout << "C::AHoge" << endl;
+    }
+
+    virtual void BHoge() {
+      cout << "C::BHoge" << endl;
+    }
+};
+
+int main(){
+  C c;
+
+  ABase& a = c;
+  a.AHoge();
+
+  BBase& b = c;
+  b.BHoge();
+}
+
