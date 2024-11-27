5b20b61e729e050515b263ef5ff58c196106e76d
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-11-27 17:09:59 +0900

    added

---
+++ Deeper/13-04/virtual_inherit2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class ABase {
+public:
+  ABase(int x): m_x(x) {}
+  virtual ~ABase() {}
+  // virtual modifier is required for override feature.
+  // = 0 is for pure virtual function that does not require implementation
+
+public:
+  void Show() { cout << m_x << endl; }
+
+
+protected:
+  int m_x;
+};
+
+class A1: virtual public ABase
+{
+public:
+  A1(): ABase(1) {}
+};
+class A2: virtual public ABase
+{
+public:
+  A2(): ABase(2) {}
+};
+
+class C:
+  public A1,
+  public A2
+{
+public:
+  C(): ABase(3) {}
+};
+
+int main(){
+  A1 a1; a1.Show();
+  A2 a2; a2.Show();
+  C c; c.Show();
+}
+
