c6429de7fa03d4e49a90f847ed42c6445b07d504
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-12-13 21:38:23 +0900

    added

---
+++ More_Deeper/14-06/variable_argument.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdarg>
+using namespace std;
+
+int Sum(int first, ...) {
+  int sum = 0;
+
+  va_list args;
+  va_start(args, first);
+  for(int n = first; n != 0; n = va_arg(args, int)){
+    sum += n;
+  }
+  va_end(args);
+
+  return sum;
+}
+
+int main(){
+  cout << Sum(0) << ' '
+      << Sum(1, 0) << ' '
+      << Sum(7, 3, 0) << ' '
+      << Sum(1, 2, 3, 4, 5, 0) << endl;
+}
