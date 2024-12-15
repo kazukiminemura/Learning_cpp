4e50ca10b163d1f5bafed227f6840b357dff9743
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-12-15 22:55:54 +0900

    added

--- More_Deeper/14-10/alignment.cpp
+++ More_Deeper/14-10/alignment.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+#pragma pack(1) // change alignment size: can be set as 1, 2, 4, 8, 16
 struct Person{
   char name[21];
   int age;
   char birthmonth;
   char sex;
 };
+#pragma pack()
 
 int main(){
-  cout << sizeof(Person) << endl;
+  cout << "      name : " << offsetof(Person, name)       << endl
+      << "        age : " << offsetof(Person, age)        << endl
+      << " birthmonth : " << offsetof(Person, birthmonth) << endl
+      << "        sex : " << offsetof(Person, sex)        << endl
+      << "       size : " << sizeof(Person)               << endl;
 }
