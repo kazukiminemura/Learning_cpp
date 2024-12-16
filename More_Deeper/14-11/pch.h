a517754c75a1960d01849496c6b67505e0929b16
Kazuki Minemura <kazuki.minemura@gmail.com>
2024-12-16 18:44:18 +0900

    added

--- More_Deeper/14-11/pch.h
+++ More_Deeper/14-11/pch.h
@@ -2,9 +2,11 @@
 #define PCH_H_
 
 #ifdef NDEBUG
-#include "Release/gen_pch.h"
+// #include "Release/gen_pch.h"
+#include "pch_base.h"
 #else
-#include "Debug/gen_pch.h"
+// #include "Debug/gen_pch.h"
+#include "pch_base.h"
 #endif
 
 #endif // #ifndef PCH_H_
